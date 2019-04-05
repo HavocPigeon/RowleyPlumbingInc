1c46d15c2c667189499ef843c5fe2c98d7a4ef94
speeas <speeas@gmail.com>
2019-04-04 17:31:25 -0700

    timeline functionality completed, js added

--- src/components/about.js
+++ src/components/about.js
@@ -6,57 +6,57 @@ export default class About extends Component {
     constructor(){
         super();
         this.state = {
-            circleOne: 'active',
-            circleTwo: 'inactive',
-            circleThree: 'inactive',
-            circleFour: 'inactive',
-            circleFive: 'inactive',
+            triangleOne: 'active',
+            triangleTwo: 'inactive',
+            triangleThree: 'inactive',
+            triangleFour: 'inactive',
+            triangleFive: 'inactive',
         }
     }
 
-    activateCircleOne = () => {
+    activatetriangleOne = () => {
         this.setState({
-            circleOne: 'active',
-            circleTwo: 'inactive',
-            circleThree: 'inactive',
-            circleFour: 'inactive',
-            circleFive: 'inactive',
+            triangleOne: 'active',
+            triangleTwo: 'inactive',
+            triangleThree: 'inactive',
+            triangleFour: 'inactive',
+            triangleFive: 'inactive',
         })
     }
-    activateCircleTwo = () => {
+    activatetriangleTwo = () => {
         this.setState({
-            circleOne: 'inactive',
-            circleTwo: 'active',
-            circleThree: 'inactive',
-            circleFour: 'inactive',
-            circleFive: 'inactive',
+            triangleOne: 'inactive',
+            triangleTwo: 'active',
+            triangleThree: 'inactive',
+            triangleFour: 'inactive',
+            triangleFive: 'inactive',
         })
     }
-    activateCircleThree = () => {
+    activatetriangleThree = () => {
         this.setState({
-            circleOne: 'inactive',
-            circleTwo: 'inactive',
-            circleThree: 'active',
-            circleFour: 'inactive',
-            circleFive: 'inactive',
+            triangleOne: 'inactive',
+            triangleTwo: 'inactive',
+            triangleThree: 'active',
+            triangleFour: 'inactive',
+            triangleFive: 'inactive',
         })
     }
-    activateCircleFour = () => {
+    activatetriangleFour = () => {
         this.setState({
-            circleOne: 'inactive',
-            circleTwo: 'inactive',
-            circleThree: 'inactive',
-            circleFour: 'active',
-            circleFive: 'inactive',
+            triangleOne: 'inactive',
+            triangleTwo: 'inactive',
+            triangleThree: 'inactive',
+            triangleFour: 'active',
+            triangleFive: 'inactive',
         })
     }
-    activateCircleFive = () => {
+    activatetriangleFive = () => {
         this.setState({
-            circleOne: 'inactive',
-            circleTwo: 'inactive',
-            circleThree: 'inactive',
-            circleFour: 'inactive',
-            circleFive: 'active',
+            triangleOne: 'inactive',
+            triangleTwo: 'inactive',
+            triangleThree: 'inactive',
+            triangleFour: 'inactive',
+            triangleFive: 'active',
         })
     }
 
@@ -73,51 +73,61 @@ export default class About extends Component {
                 </div>
                 <div className="timelinecontainer">
                     <div className="timelineleft">
-                        <button id={
-                            this.state.circleOne === 'active' 
-                            ? 'circleactive'
-                            : 'circle'
-                        } onClick={() => this.activateCircleOne()}></button>
+                        <button onClick={() => this.activatetriangleOne()}>
+                            <div id={
+                                this.state.triangleOne === 'active' 
+                                ? 'triangleactive'
+                                : 'triangle'
+                            }></div>
+                        </button>
                         <div id={
-                            this.state.circleOne === 'inactive' 
+                            this.state.triangleOne === 'inactive' 
                             ? 'timelinelengthactive'
                             : 'timelinelength'
                         }></div>
-                        <button id={
-                            this.state.circleTwo === 'active' 
-                            ? 'circleactive'
-                            : 'circle'
-                        } onClick={() => this.activateCircleTwo()}></button>
+                        <button onClick={() => this.activatetriangleTwo()}>
+                            <div id={
+                            this.state.triangleTwo === 'active' 
+                            ? 'triangleactive'
+                            : 'triangle'
+                            }></div>
+                        </button>
                         <div id={
-                            this.state.circleOne || this.state.circleTwo === 'active' 
-                            ? 'timelinelength'
-                            : 'timelinelengthactive'
-                        }></div>
-                        <button id={
-                            this.state.circleThree === 'active' 
-                            ? 'circleactive'
-                            : 'circle'
-                        } onClick={() => this.activateCircleThree()}></button>
-                        <div id={
-                            this.state.circleOne || this.state.circleTwo || this.state.circleThree === 'active' 
-                            ? 'timelinelength'
-                            : 'timelinelengthactive'
-                        }></div>
-                        <button id={
-                            this.state.circleFour === 'active' 
-                            ? 'circleactive'
-                            : 'circle'
-                        } onClick={() => this.activateCircleFour()}></button>
-                        <div id={
-                            this.state.circleFive === 'active' 
+                            this.state.triangleTwo === 'inactive' && this.state.triangleOne === 'inactive'
                             ? 'timelinelengthactive'
                             : 'timelinelength'
                         }></div>
-                        <button id={
-                            this.state.circleFive === 'active' 
-                            ? 'circleactive'
-                            : 'circle'
-                        } onClick={() => this.activateCircleFive()}></button>
+                        <button onClick={() => this.activatetriangleThree()}>
+                            <div id={
+                            this.state.triangleThree === 'active' 
+                            ? 'triangleactive'
+                            : 'triangle'
+                            }></div>
+                        </button>
+                        <div id={
+                            this.state.triangleFour === 'active' || this.state.triangleFive === 'active'
+                            ? 'timelinelengthactive'
+                            : 'timelinelength'
+                        }></div>
+                        <button onClick={() => this.activatetriangleFour()}>
+                        <div id={
+                            this.state.triangleFour === 'active' 
+                            ? 'triangleactive'
+                            : 'triangle'
+                            }></div>
+                        </button>
+                        <div id={
+                            this.state.triangleFive === 'active' 
+                            ? 'timelinelengthactive'
+                            : 'timelinelength'
+                        }></div>
+                        <button onClick={() => this.activatetriangleFive()}>
+                            <div id={
+                            this.state.triangleFive === 'active' 
+                            ? 'triangleactive'
+                            : 'triangle'
+                            }></div>
+                        </button>
                     </div>
                     <div className="timelineright">
                         There
