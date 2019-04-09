51f576afb62fe284d05dd2032b1d804720987bc7
speeas <speeas@gmail.com>
2019-04-08 18:58:01 -0700

    hiddennavbar added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import '../css/navbar.css';
 import Homelogo from '../Images/Final Logo.png';
-
+import Hiddenmenubutton from '../Images/Icons/Hiddenmenubutton.png';
 
 export default class Navbar extends Component {
+    constructor(){
+        super();
+        this.state = {
+            hiddenmenu: false,
+        }
+    }
 
     scrollToTop = () => {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
+        this.setState({
+            hiddenmenu: false,
+        })
+    }
+    openHiddenMenu = () => {
+        this.setState({
+            hiddenmenu: !this.state.hiddenmenu,
+        })
     }
 
     render() {
@@ -43,6 +57,41 @@ export default class Navbar extends Component {
                     ? "contacth"
                     : "contactsh"
                 }>Contact</a>
+                <div className={
+                    this.props.scrollposition <= 0 
+                    ? "hiddenmenubuttonh"
+                    : "hiddenmenubuttonsh"
+                }>
+                    <button onClick={() => {
+                        this.openHiddenMenu();
+                    }}>
+                        <img className='hiddenmenuimage' src={Hiddenmenubutton} alt="hiddenmenubutton"/>
+                    </button>
+                </div>
+
+                <div className={
+                    this.state.hiddenmenu === true && this.props.scrollposition > 0
+                    ? 'hiddennavbar'
+                    : 'hiddennavbarh'
+                }>
+                    <a href="#about" className={
+                        this.state.hiddenmenu === true && this.props.scrollposition > 0
+                        ? 'abouthiddenmenu'
+                        : 'abouthiddenmenuh'
+                    }>About</a>
+                    <a href="#projects"  className={
+                        this.state.hiddenmenu === true && this.props.scrollposition > 0
+                        ? 'projecthiddenmenu'
+                        : 'projecthiddenmenuh'
+                    }>Projects</a>
+                    <a href="#contact"  className={
+                        this.state.hiddenmenu === true && this.props.scrollposition > 0
+                        ? 'contacthiddenmenu'
+                        : 'contacthiddenmenuh'
+                    }>Contact</a>
+                    
+                </div>
+
             </div>
         );
     }
